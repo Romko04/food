8af504c0acdab029f943b6f65452173138a6a125
Roman <kurpelroma@gmail.com>
2023-12-14 14:31:53 +0200

    change nav on slider

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper', {
+    let swiper = new Swiper('.foods__wrapper', {
         slidesPerView: 1.3,
         spaceBetween: 20,
         centeredSlides: true,
     });
 
-    var today = new Date().getDay();
+    let today = new Date().getDay();
     
     swiper.slideTo(today - 1, 1000, true);
+
+    let swiperNav = new Swiper('.nav__wrapper', {
+        slidesPerView: 4,
+        spaceBetween: 10,
+    });
+    
 });
 
 
-const btnYet = document.querySelector('.footer__navigation-list__item-link--yet')
-const links = document.querySelectorAll('.footer__navigation-list__item-link')
+// const btnYet = document.querySelector('.footer__navigation-list__item-link--yet')
+// const links = document.querySelectorAll('.footer__navigation-list__item-link')
 
-if (links.length > 4) {
-    btnYet.classList.add('active')
-    for (let i = 3; i < links.length; i++) {
-        links[i].classList.add('hidden');
-    }
-}
+// if (links.length > 4) {
+//     btnYet.classList.add('active')
+//     for (let i = 3; i < links.length; i++) {
+//         links[i].classList.add('hidden');
+//     }
+// }
 
-btnYet.addEventListener('click',(e)=>{
-    e.preventDefault()
-    const list = document.querySelector('.footer__navigation-list')
-    list.classList.add('active')
-    btnYet.classList.remove('active')
-    for (let i = 3; i < links.length; i++) {
-        links[i].classList.add('active');
-    }
-})
+// btnYet.addEventListener('click',(e)=>{
+//     e.preventDefault()
+//     btnYet.classList.remove('active')
+//     for (let i = 3; i < links.length; i++) {
+//         links[i].classList.add('active');
+//     }
+// })
