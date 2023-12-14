b86e2f185dc12b03a5a38f42514950935e2f3176
Roman <kurpelroma@gmail.com>
2023-12-14 11:33:18 +0200

    add script for scrolling to day of the week

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-new Swiper('.swiper', {
-    slidesPerView: 1.3,
-    spaceBetween: 20,
-    centeredSlides: true,
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper', {
+        slidesPerView: 1.3,
+        spaceBetween: 20,
+        centeredSlides: true,
+    });
+
+    var today = new Date().getDay();
+    
+    swiper.slideTo(today - 1, 1000, true);
 });
