5e7ba1c6c0c72409c8129a8103e09d82cf088a95
Roman <kurpelroma@gmail.com>
2023-12-14 13:22:43 +0200

    Created a script for logic btn "yet"

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
     
     swiper.slideTo(today - 1, 1000, true);
 });
+
+
+const btnYet = document.querySelector('.footer__navigation-list__item-link--yet')
+const links = document.querySelectorAll('.footer__navigation-list__item-link')
+
+if (links.length > 3) {
+    btnYet.classList.add('active')
+    for (let i = 3; i < links.length; i++) {
+        links[i].classList.add('hidden');
+    }
+}
+
+btnYet.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const list = document.querySelector('.footer__navigation-list')
+    list.classList.add('active')
+    btnYet.classList.remove('active')
+    for (let i = 3; i < links.length; i++) {
+        links[i].classList.add('active');
+    }
+})
